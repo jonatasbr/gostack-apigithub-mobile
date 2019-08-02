8e4508232503e48268571dab7dc7d7d6d83ffbd7
Jonatas Braz <jonatasbraz@gmail.com>
2019-08-02 02:29:56 -0300

    feature: navegacao

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-community/async-storage';
 import { Keyboard, ActivityIndicator } from 'react-native';
@@ -7,6 +8,16 @@ import api from '../../sevices/api';
 import { Container, Form, Input, SubmitButton, List, User, Name, Avatar, Bio, ProfileButton, ProfileButtonText, } from './styles';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
@@ -14,6 +25,7 @@ export default class Main extends Component {
   };
 
   async componentDidMount() {
+    console.tron.log(this.props);
     const users = await AsyncStorage.getItem('users');
 
     if (users) {
@@ -52,6 +64,12 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigate = (user) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
   render() {
     const { users, newUser, loading } = this.state;
 
@@ -83,7 +101,7 @@ export default class Main extends Component {
               <Avatar source={{ uri: item.avatar }} />
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -93,7 +111,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
